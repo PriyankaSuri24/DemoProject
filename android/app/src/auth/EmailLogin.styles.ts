a2bf1bd17127382c305737520d3a5be04c115083
Priyanka Suri <psuri@radicleinc.com>
2026-01-16 16:16:33 +0530

    Email Login tab

---
+++ android/app/src/auth/EmailLogin.styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    view: {
+        backgroundColor: '#dbaaef',
+    },
+    container: {
+        justifyContent: 'center',
+        marginVertical: 20,
+        gap: 12,
+    },
+    input: {
+        marginHorizontal: 20,
+        marginVertical: 8,
+        padding: 14,
+        fontSize: 16,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#af45d9',
+    },
+    loginButton: {
+        marginTop: 20,
+        marginHorizontal: 20,
+        paddingVertical: 14,
+        backgroundColor: '#af45d9',
+        borderRadius: 10,
+        alignItems: 'center',
+    }
+})
---
+++ android/app/src/auth/EmailLogin.tsx
@@ -0,0 +1,47 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/authContext";
+import { getUsers } from "../storage/userStorage";
+import { validateEmail } from "../utils/validation";
+import { Alert, ScrollView, View, Text, Pressable, TextInput } from "react-native";
+import React from "react";
+import { styles } from './EmailLogin.styles';
+
+export default function EmailLogin() {
+    const { login } = useContext(AuthContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        const users = await getUsers();
+        if(!email) return Alert.alert("Error", "Email required.");
+        if (email && !validateEmail(email)) return Alert.alert("Error", "Invalid email format.");
+        const user = users.find((u: { email: string; }) => u.email === email);
+        if (!user) return Alert.alert("Error", "User not found");
+        if (user.password !== password) return Alert.alert('Error', 'Incorrect password');
+        await login({ userId: user.id, loginTime: new Date().toISOString() });
+    }
+
+    return (
+        <ScrollView style={styles.view}>
+            <View style={styles.container}>
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                    style={styles.input}
+                />
+                <Pressable onPress={handleLogin} style={styles.loginButton}>
+                    <Text>Login</Text>
+                </Pressable>
+            </View>
+        </ScrollView>
+    )
+}
