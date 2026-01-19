dfbb3764518b35beedf9ff7217c5452fba6ffd10
Priyanka Suri <psuri@radicleinc.com>
2026-01-19 19:33:13 +0530

    add message on top of home screen

---
+++ android/app/src/app/Greeting.tsx
@@ -0,0 +1,19 @@
+import { Text } from "react-native";
+import { styles } from './Greetings.styles';
+
+export default function Greeting(){
+    const hour = new Date().getHours();
+    let message = '';
+
+    if (hour >= 5 && hour < 12){
+        message = "🌤️ Good Morning! Let's start organizing your task and budget."; 
+    }else if(hour >= 12 && hour < 17){
+        message = "Good Afternoon 📈! Keep up with your plan";
+    }else if(hour >= 17 && hour < 21){
+        message = "Good Evening! Time to wrap up your day ✅";
+    }else{
+        message = "Ready for some 🌙🌙 night planning?";
+    }
+
+    return <Text style={styles.message}>{message}</Text>
+}
