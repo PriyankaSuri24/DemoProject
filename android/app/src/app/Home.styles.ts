d9c6e287f229d561df9bf65b5e013cfd9657a3b1
Priyanka Suri <psuri@radicleinc.com>
2026-01-19 19:34:49 +0530

    remove logout navigation

---
+++ android/app/src/app/Home.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create ({
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems : 'center',
+        backgroundColor: '#dbaaef',
+    }, 
+    text : {
+        fontSize : 20,
+        fontWeight : 'bold',
+        color: '#fff',
+    },
+    logout: {
+        fontSize : 15,
+        fontWeight : 'bold',
+        color: '#fff',
+    }
+})
