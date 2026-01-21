de851964901f3fccf0e1360e31d1edb62725f924
Priyanka Suri <psuri@radicleinc.com>
2026-01-21 12:13:37 +0530

    add styles to the settings page

---
+++ android/app/src/app/AppSettings.styles.ts
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native";
+import { LightNavTheme, BlueDarkNavTheme } from "../theme/appThemes";
+import { ThemeContext } from "../context/ThemeContext";
+import { useContext } from "react";
+
+export const useStyles = () => {
+    const { theme } = useContext(ThemeContext);
+    const colors = theme === "light" ? LightNavTheme.colors : BlueDarkNavTheme.colors;
+
+    return StyleSheet.create({
+        container: {
+          flex: 1,
+          padding: 20,
+        },
+        heading: {
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginBottom: 24,
+          color: "#fff",
+        },
+        row: {
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginVertical: 12,
+        },
+        label: {
+          fontSize: 16,
+          color: "#fff",
+        },
+        logoutButton: {
+          marginTop: 480,
+          marginLeft: 270,
+          width: "30%",
+          backgroundColor: colors.border, 
+          paddingVertical: 14,
+          borderRadius: 12,
+          alignItems: "center",
+          justifyContent: "center",
+          elevation: 4
+        },
+        logoutText: {
+          fontSize: 18,
+          fontWeight: "600",
+          color: colors.text,
+        }
+    });
+};
