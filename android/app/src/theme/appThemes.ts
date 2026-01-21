64d55456c501f313a6b50c16e9d4bef881056d69
Priyanka Suri <psuri@radicleinc.com>
2026-01-21 12:09:05 +0530

    define purple and blue theme

---
+++ android/app/src/theme/appThemes.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme, DarkTheme, Theme as NavigationTheme } from '@react-navigation/native';
+
+export const LightNavTheme: NavigationTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#dbaaef',
+    card: '#af45d9',
+    text: '#fff',
+    border: '#af45d9',
+  },
+};
+
+export const BlueDarkNavTheme: NavigationTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    background: '#8e9ef8',
+    card: '#0420be',
+    text: '#fff',
+    border: '#0420be',
+  },
+};
