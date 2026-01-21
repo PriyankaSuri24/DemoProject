1f50ef26e96b7355c8983813e7d86453dbbe6b7f
Priyanka Suri <psuri@radicleinc.com>
2026-01-21 12:05:34 +0530

    implement themes and theme provider to whole app

--- index.tsx
+++ index.tsx
@@ -1,13 +1,28 @@
 import { AppRegistry } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import App from './App';
+import { LightNavTheme, BlueDarkNavTheme } from './android/app/src/theme/appThemes';
+import { enableScreens } from 'react-native-screens';
 import { name as appName } from './app.json';
+import { ThemeProvider, ThemeContext } from './android/app/src/context/ThemeContext';
+import React, { useContext } from 'react';
+
+enableScreens();
+
+function MainNavigation() {
+  const { theme } = useContext(ThemeContext); 
+  return (
+    <NavigationContainer theme={theme === 'light' ? LightNavTheme : BlueDarkNavTheme}>
+      <App />
+    </NavigationContainer>
+  );
+}
 
 function Main() {
   return (
-    <NavigationContainer>
-      <App />
-    </NavigationContainer>
+    <ThemeProvider>
+      <MainNavigation />
+    </ThemeProvider>
   );
 }
 
