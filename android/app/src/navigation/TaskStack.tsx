cf6024c95b4c2d4b19e493252990aa5d6d4a5d34
Priyanka Suri <psuri@radicleinc.com>
2026-01-21 19:30:47 +0530

    created 2 screens on task manager

--- android/app/src/navigation/TaskStack.tsx
+++ android/app/src/navigation/TaskStack.tsx
@@ -1,7 +1,38 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TaskListScreen from "../screens/tasks/TaskListScreen";
+import AddEditTaskScreen from "../screens/tasks/AddEditTaskScreen";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { BlueDarkNavTheme, LightNavTheme } from "../theme/appThemes";
+
+const Stack = createNativeStackNavigator();
 
 export default function TaskStack(){
+    const { theme } = useContext(ThemeContext);
+    const colors = theme === "light" ? LightNavTheme.colors : BlueDarkNavTheme.colors;
     return(
-        <>
-        </>
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: colors.primary,
+                },
+                headerTintColor: colors.text, 
+                headerTitleAlign: "center",
+                headerTitleStyle: {
+                    fontWeight: "bold",
+                },
+            }}
+        >
+            <Stack.Screen
+                name="TaskList"
+                component={TaskListScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="AddEditTask"
+                component={AddEditTaskScreen}
+                options={{title: "Add Task"}}
+            />
+        </Stack.Navigator>
     );
 }
