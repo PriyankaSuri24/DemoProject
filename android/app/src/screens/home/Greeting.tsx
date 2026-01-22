e1d337f92f148cf284c6ef8c47be7399c722881e
Priyanka Suri <psuri@radicleinc.com>
2026-01-22 16:54:11 +0530

    removed emoji's

--- android/app/src/screens/home/Greeting.tsx
+++ android/app/src/screens/home/Greeting.tsx
@@ -6,13 +6,13 @@ export default function Greeting(){
     let message = '';
 
     if (hour >= 5 && hour < 12){
-        message = "🌤️ Good Morning! Let's start organizing your task and budget."; 
+        message = "Good Morning! Let's start organizing your task and budget."; 
     }else if(hour >= 12 && hour < 17){
-        message = "Good Afternoon 📈! Keep up with your plan";
+        message = "Good Afternoon! Keep up with your plan";
     }else if(hour >= 17 && hour < 21){
-        message = "Good Evening! Time to wrap up your day ✅";
+        message = "Good Evening! Time to wrap up your day";
     }else{
-        message = "Ready for some 🌙🌙 night planning?";
+        message = "Ready for some night planning?";
     }
 
     return <Text style={styles.message}>{message}</Text>
